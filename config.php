be7c4f9411ba80f3e23b687e47258fb17b03599a
Christopher Pitt <cgpitt@gmail.com>
2017-08-10 05:53:05 +0200

    Feedback changes

--- config.php
+++ config.php
@@ -10,8 +10,9 @@ use function Aerys\router;
 $host = new Host();
 $host->expose("*", 8888);
 
-$host->use($root = root(__DIR__ . "/public"));
 $host->use($router = router());
 
 $routes = require __DIR__ . "/routes.php";
 $routes($router);
+
+$host->use($root = root(__DIR__ . "/public"));
